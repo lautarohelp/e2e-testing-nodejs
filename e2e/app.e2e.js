d91a94524ce4aa5a15a3acc94bdf3389851ddd1c
lautarohelp <lautarotoledo391@gmail.com>
2024-01-16 14:44:58 -0600

    Buenas prácticas en pruebas E2E

--- e2e/app.e2e.js
+++ e2e/app.e2e.js
@@ -1,21 +1,33 @@
 const  request  = require('supertest');
 const  express  = require('express');
 
-const app = express();
-
-app.get('/hello', (req, res) => {
-  res.status(200).json({ name: 'lauti' });
-})
-
-app.listen(9000);
-
-const api = request(app);
-
 describe('test for app', () => {
+
+  let app = null;
+  let server = null;
+  let api = null;
+
+  beforeEach(() => {
+    app = express();
+
+    app.get('/hello', (req, res) => {
+      res.status(200).json({ name: 'lauti' });
+    })
+
+    server = app.listen(9000);
+    api = request(app);
+  });
+
+
+
   test('GET /hello', async () => {
     const response = await api.get('/hello');
     expect(response).toBeTruthy();
     expect(response.body.name).toEqual('lauti');
     expect(response.headers['content-type']).toMatch(/json/);
   });
-})
+
+  afterEach(() => {
+    server.close();
+  });
+});
